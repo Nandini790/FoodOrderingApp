d10e73d0be2a5bfae9a8a482c7024c7ca8fabe7c
Nandini Singh <nandinisingh.creator@gmail.com>
2026-07-15 01:08:54 +0530

    material added with thememkdir -p src/app/core/guardsmkdir -p src/app/core/guards

---
+++ src/app/features/auth/guest-checkout/guest-checkout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guest-checkout',
+  imports: [],
+  templateUrl: './guest-checkout.html',
+  styleUrl: './guest-checkout.scss',
+})
+export class GuestCheckout {
+
+}
